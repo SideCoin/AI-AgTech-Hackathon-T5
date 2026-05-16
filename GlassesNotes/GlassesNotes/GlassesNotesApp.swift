2180d199abae884628ac3e5df98101ebac2c6ea2
Regan Yang <yreganyang@gmail.com>
2026-05-15 22:08:16 -0700

    proj template

---
+++ GlassesNotes/GlassesNotes/GlassesNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesNotesApp.swift
+//  GlassesNotes
+//
+//  Created by Shizun Yang on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct GlassesNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlassesNotes/GlassesNotesTests/GlassesNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesNotesTests.swift
+//  GlassesNotesTests
+//
+//  Created by Shizun Yang on 5/15/26.
+//
+
+import Testing
+@testable import GlassesNotes
+
+struct GlassesNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
